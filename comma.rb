19bc73390a30b44b95a341a980380c3f70a544e0
sjulier <S.Julier@cs.ucl.ac.uk>
2014-06-18 09:54:28 +0100

    Fixed build settings. Added dependencies. Disable applications for now.

--- comma.rb
+++ comma.rb
@@ -4,22 +4,18 @@ require "formula"
 #                /usr/local/Library/Contributions/example-formula.rb
 
 class Comma < Formula
-  homepage ""
-  url "https://github.com/acfr/comma.git"
+  homepage "https://github.com/acfr/comma/wiki"
+  url "https://github.com/acfr/comma.git", :revision => "16526682a3b5a56996754c1d912872a4023356a3"
   sha1 ""
+  version "1.0.5"
 
   depends_on "cmake" => :build
+  depends_on "boost" => :build
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-
-    # Remove unrecognized options if warned by configure
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
-    # system "cmake", ".", *std_cmake_args
-    system "make", "install" # if this fails, try separate make/make install steps
+    system "mkdir build"
+    cd 'build'
+    system "cmake", "..", "-DCMAKE_BUILD_TYPE=Release", "-DBUILD_APPLICATIONS=OFF", "-DBUILD_TESTS=OFF", "-DBUILD_SHARED_LIBS=ON", *std_cmake_args
+    system "make install"
   end
-
 end
