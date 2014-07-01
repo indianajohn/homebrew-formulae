5c645280b765f8b7d0189f46858d4bca987a574b
sjulier <S.Julier@cs.ucl.ac.uk>
2014-07-01 10:09:32 +0100

    Install headers in include/gpstk to be consistent with CRATES.

--- gpstk.rb
+++ gpstk.rb
@@ -11,8 +11,30 @@ class Gpstk < Formula
 
   depends_on 'cmake' => :build
 
+  patch :DATA
+
   def install
     system "cmake", "dev", *std_cmake_args
     system "make install"
   end
 end
+
+__END__
+
+diff --git a/dev/CMakeLists.txt b/dev/CMakeLists.txt
+index 9b510d0..4f99993 100755
+--- a/dev/CMakeLists.txt
++++ b/dev/CMakeLists.txt
+@@ -187,9 +187,9 @@ endif()
+ 
+ install (TARGETS gpstk DESTINATION lib)
+ if (CORE_ONLY)
+-  install (FILES ${CORE_HDRS} ${CORE_HDRS2} DESTINATION include )
++  install (FILES ${CORE_HDRS} ${CORE_HDRS2} DESTINATION include/gpstk )
+ else ()
+-  install (FILES ${CORE_HDRS} ${CORE_HDRS2} ${EXT_HDRS} ${EXT_HDRS2} DESTINATION include )
++  install (FILES ${CORE_HDRS} ${CORE_HDRS2} ${EXT_HDRS} ${EXT_HDRS2} DESTINATION include/gpstk )
+ endif ()
+ 
+ 
+
