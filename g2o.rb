9df34c394607a98b2fcef1a4f98e962169aa4ff6
sjulier <S.Julier@cs.ucl.ac.uk>
2015-08-02 07:18:25 -0600

    Added g2o formula from https://github.com/RainerKuemmerle/g2o/issues/25

---
+++ g2o.rb
@@ -0,0 +1,58 @@
+require 'formula'
+
+class G2o < Formula
+  homepage 'http://openslam.org/g2o.html'
+  url 'https://github.com/RainerKuemmerle/g2o.git', :branch => 'master'
+  version '13.'
+
+  depends_on 'cmake'
+  depends_on 'eigen'
+  depends_on 'suite-sparse'
+  depends_on 'qt'
+  depends_on 'libqglviewer'
+
+  def options
+    [
+      ['--no_apps',"Don't build applications"],
+      ['--deprecated', "Build deprecated types"],
+      ['--examples', "Build examples"],
+      ['--linked', "Build linked applications"],
+      ['--csparse', "Build csparse"],
+      ['--no_opengl',"Disable use opengl"],
+      ['--use_openmp', "Use openmp"],
+    ]
+  end
+
+  def install
+    args = std_cmake_parameters.split
+
+    if ARGV.include? '--no_apps'
+      args << "-DG2O_BUILD_APPS:BOOL=OFF"
+    end
+    if ARGV.include? '--deprecated'
+      args << "-DG2O_BUILD_DEPRECATED_TYPES:BOOL=ON"
+    end
+    if ARGV.include? '--examples'
+      args << "-DG2O_BUILD_EXAMPLES:BOOL=OFF"
+    end
+    if ARGV.include? '--linked'
+      args << "-DG2O_BUILD_LINKED_APPS:BOOL=ON"
+    end
+    if ARGV.include? '--csparse'
+      args << "-DBUILD_CSPARSE:BOOL=ON"
+    end
+    if ARGV.include? '--no_opengl'
+      args << "DG2O_USE_OPENGL:BOOL=OFF"
+    end
+    if ARGV.include? '--use_opengl'
+      args << "DG2O_USE_OPENMP:BOOL=ON"
+    end
+
+    system "mkdir build"
+    args << ".."
+    Dir.chdir 'build' do
+      system "cmake", *args
+      system "make install"
+    end
+  end
+end
